281d06b37497e3d3efd3829907c23650ee075a72
Richard Martin <richardnv@gmail.com>
2024-07-19 11:45:02 -0400

    Django Final

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -128,6 +128,7 @@ def extract_answers(request):
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
 def show_exam_result(request, course_id, submission_id):
+    context = {}
     course = get_object_or_404(Course, pk=course_id)
     submission = get_object_or_404(Submission, pk=submission_id)
     choices = submission.choices.all()
